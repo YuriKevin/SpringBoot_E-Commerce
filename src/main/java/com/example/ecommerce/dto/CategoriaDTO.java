775aca31ef96acd06e39f7b2ea5b381b53064ca5
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:47:24 -0300

    feat: classe DTO de Categoria

---
+++ src/main/java/com/example/ecommerce/dto/CategoriaDTO.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce.dto;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoriaDTO {
+	private String titulo;
+    private List<ProdutoDTO> produtos;
+}
