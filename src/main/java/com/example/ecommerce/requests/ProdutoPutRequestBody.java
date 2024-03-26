9fcba3d8e2545d0aebaaecdca80a90c45a803600
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:46:43 -0300

    feat: classe para PUT de um Produto

---
+++ src/main/java/com/example/ecommerce/requests/ProdutoPutRequestBody.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce.requests;
+import java.util.List;
+
+import com.example.ecommerce.model.DetalhesProduto;
+
+import lombok.Data;
+
+@Data
+public class ProdutoPutRequestBody {
+	private Long id;
+	private String titulo;
+	private Double valor;
+	private Long quantidade;
+	private String categoria;
+	private List<String> imagens;
+	private List<DetalhesProduto> detalhes;
+	private boolean disponivel;
+}
