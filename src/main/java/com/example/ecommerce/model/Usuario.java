7611599d9581e178d1a8288151edbb2bd611542f
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:38:49 -0300

    feat: Classe modelo de Usuario

---
+++ src/main/java/com/example/ecommerce/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Usuario {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String nome;
+	@Column(unique = true)
+	private String email;
+	private String senha;
+	private Double credito;
+}
