d1ea301c6a0a77ff110dfb1d5718ce9ee8551d64
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:42:54 -0300

    feat: classe modelo de  DetalhesPreConfigurados

---
+++ src/main/java/com/example/ecommerce/model/DetalhesPreConfigurados.java
@@ -0,0 +1,30 @@
+package com.example.ecommerce.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class DetalhesPreConfigurados {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String titulo;
+	@OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "detalhesPreConfigurados_id")
+    private List<DetalhesProduto> detalhes;
+}
