984075800c469ca56862ba38cc78c5dba76d78d2
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:39:59 -0300

    feat: Classe modelo de Categoria

---
+++ src/main/java/com/example/ecommerce/model/Categoria.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Categoria {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	@OneToMany(mappedBy = "categoria")
+    private List<Produto> produtos;
+}
