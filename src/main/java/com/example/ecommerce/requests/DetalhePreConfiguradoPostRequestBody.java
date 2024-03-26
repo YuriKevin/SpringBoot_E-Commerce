811f742f790ea905f51a15df5ba19fda1f2898ed
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:45:46 -0300

    feat: classe para POST de um DetalhePreConfigurado

---
+++ src/main/java/com/example/ecommerce/requests/DetalhePreConfiguradoPostRequestBody.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.requests;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class DetalhePreConfiguradoPostRequestBody {
+	private String titulo;
+    private List<DetalhesProdutoPostRequestBody> detalhes;
+}
