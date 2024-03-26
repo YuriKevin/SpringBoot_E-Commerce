638ed7579f52e20e18e3ce6d37fe89f3900ee3d0
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:47:06 -0300

    feat: classe DTO de Produto

---
+++ src/main/java/com/example/ecommerce/dto/ProdutoDTO.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.dto;
+
+import java.util.List;
+import com.example.ecommerce.model.DetalhesProduto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProdutoDTO {
+	private Long id;
+	private String titulo;
+	private Double valor;
+    private List<String> imagens;
+    private List<DetalhesProduto> detalhes;
+    private Long quantidade;
+	private Long quantidadeVendida;
+	private Double avaliacao;
+	private boolean disponivel;
+}
