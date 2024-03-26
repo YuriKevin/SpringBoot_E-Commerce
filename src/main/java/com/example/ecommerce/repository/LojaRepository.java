4171ab30e3d0d1bcbf471a0a452908df4e94df0f
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 13:42:01 -0300

    feat: classe Repository de Loja

---
+++ src/main/java/com/example/ecommerce/repository/LojaRepository.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.repository;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.ecommerce.model.Loja;
+
+public interface LojaRepository extends JpaRepository<Loja, Long>{
+	Loja findByCodigoLogin(Long codigoLogin);
+	List<Loja> findByNome(String nome, Pageable page);
+	
+	@Query("SELECT l FROM Loja l ORDER BY l.quantidadeVendida DESC")
+    List<Loja> findTop20ByOrderByQuantidadeVendidaDesc();
+}
