cfa8abe780ea5e4e299d963cbbb00262853d260b
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-01 18:42:29 -0300

    feat: definindo coluna imagem como LONGTEXT para suportar strins gigantes que representam imagens na base 64.

--- src/main/java/com/example/ecommerce/model/Produto.java
+++ src/main/java/com/example/ecommerce/model/Produto.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.model;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -32,6 +34,8 @@ public class Produto {
 	private String categoria;
 	private String nomeLoja;
 	@ElementCollection
+	@Lob
+    @Column(columnDefinition = "LONGTEXT")
     private List<String> imagens;
 	@ManyToOne(fetch = FetchType.LAZY)
     private Loja loja;
