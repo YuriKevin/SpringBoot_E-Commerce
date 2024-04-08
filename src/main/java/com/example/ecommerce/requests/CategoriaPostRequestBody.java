3da4d9505de97647579267e1eb5ff312f4bdc314
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-08 18:45:31 -0300

    feat: nova classe para post de Categoria.

---
+++ src/main/java/com/example/ecommerce/requests/CategoriaPostRequestBody.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.requests;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CategoriaPostRequestBody {
+	private String titulo;
+    private List<Long> idsProdutos;
+}
